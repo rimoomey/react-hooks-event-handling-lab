ca7c73f7308895fa12c50256927ff474aa8934d2
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-22 10:10:41 -0600

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+  return (
+    <button onFocus={console.log("Good!")} onBlur={ console.log("Hey! Eyes on me!") }>Eyes on Me!</button>
+  );
+}
+
+export default EyesOnMe;
